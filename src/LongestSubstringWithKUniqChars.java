605a4b2b20bb19e4f373c28b6a9f73c8dbd11cce
markfront <markfront@gmail.com>
2020-07-01 11:57:02 -0700

    Update LongestSubstringWithKUniqChars.java

--- src/LongestSubstringWithKUniqChars.java
+++ src/LongestSubstringWithKUniqChars.java
@@ -1,8 +1,8 @@
 /*
 This is a problem asked by Google.
 
-Given a string, find the longest substring that contains only two unique characters. 
-For example, given "abcbbbbcccbdddadacb", the longest substring that contains 2 unique character is "bcbbbbcccb". 
+Given a string, find the longest substring that contains only k unique characters. 
+For example, given "abcbbbbcccbdddadacb" and k=2, the longest substring that contains 2 unique character is "bcbbbbcccb". 
 */
 
 public class Question {
@@ -10,38 +10,35 @@ public class Question {
     if (s==null || s.length()<k) return null;
     
     char[] chars = s.toCharArray();
-    Map<Character, Integer> map = new HashMap<>();
-    int max = Integer.MIN_VALUE;
+    Map<Character, Integer> win = new HashMap<>();
+    int maxLen = Integer.MIN_VALUE;
+    
     int i=0, j=0; // 2 pointers
-    while(i<=j && j<chars.length) {
-      char c1 = chars[i];
-      char c2 = chars[j];
-      if (map.size() < k) {
-        if (!map.containsKey(c2)) map.put(c2, 1);
-        else map.put(c2, map.get(c2)+1);
+    while(i<=j && j<chars.length) {      
+      if (win.size() < k) {
+        char c2 = chars[j]; // right boundary of the window
+        // expand the window with c2
+        if (!win.containsKey(c2)) win.put(c2, 1);
+        else win.put(c2, win.get(c2)+1);
         j++;
       } else {
-        max = Math.max(max, j-i);
+        // update the maxLen
+        maxLen = Math.max(maxLen, j-i);
         
-        shiftWindow(map, c1, c2);
+        char c1 = chars[i]; // left boundary of the window
+        // try reduce or drop c1 from the window
+        if (win.get(c1) == 0) win.remove(c1);
+        else win.put(c1, win.get(c1)-1);
+        
         i++;
       }
     }
     
-    if (map.size()==k) {
-      max = Math.max(max, j-i);
+    if (win.size()==k) {
+      maxLen = Math.max(maxLen, j-i);
     }
     
-    if (max != Integer.MIN_VALUE) return s.substring(i, j-i+1);
+    if (maxLen != Integer.MIN_VALUE) return s.substring(i, j-i+1);
     return null;
-  }
-  
-  private void shiftWindow(Map<Character, Integer> map, char c1, char c2) {
-    if (map.containsKey(c1)) {
-      if (map.get(c1) == 0) map.remove(c1);
-      else map.put(c1, map.get(c1)-1);
-    }
-    if (!map.containsKey(c2)) map.put(c2, 1);
-    else map.put(c2, map.get(c2)+1);
   }
 }
