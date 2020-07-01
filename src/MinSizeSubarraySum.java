0aa1b5d506db6e535e74dacd17e4f9ed1879b05b
markfront <markfront@gmail.com>
2020-07-01 09:17:11 -0700

    Create MinSizeSubarraySum.java

---
+++ src/MinSizeSubarraySum.java
@@ -0,0 +1,30 @@
+/*
+Given an array of n positive integers and a positive integer s, find the minimal length 
+of a subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+For example, given the array [2,3,1,2,4,3] and s = 7, the subarray [4,3] 
+has the minimal length of 2 under the problem constraint.
+*/
+
+public class Question {
+  public int solve(int[] a, int s) {
+    if (a==null || a.length==0) return 0;
+    
+    int min = Integer.MAX_VALUE;
+    int sum=0, i=0, j=0; // left/right pointer
+    while(i<=j && j<a.length) {
+      if (sum<s) {
+        sum += a[j]; 
+        j++;
+      } else { // sum>=s
+        min = Math.min(min, j-i+1); // keep the min length seen so far
+        sum -= a[i];
+        i++;
+      }
+    }
+    if (sum>=s) {
+      min = Math.min(min, j-i+1);
+    }
+    return min;
+  }  
+}
