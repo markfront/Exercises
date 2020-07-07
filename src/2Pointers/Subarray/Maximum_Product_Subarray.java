c8b44b7ccd92f76542a232e1eae852eff6add390
markfront <markfront@gmail.com>
2020-07-07 14:49:38 -0700

    Update Maximum_Product_Subarray.java

--- src/2Pointers/Subarray/Maximum_Product_Subarray.java
+++ src/2Pointers/Subarray/Maximum_Product_Subarray.java
@@ -1,4 +1,21 @@
 /*
+https://leetcode.com/problems/maximum-product-subarray/
+
+Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+Example 1:
+
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+
+Example 2:
+
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+
 Idea 1: 
 Loop through the array twice. One goes forward, the other goes backward. Do product and update max each loop. 
 This should be enough to cover products of all subarrays.
