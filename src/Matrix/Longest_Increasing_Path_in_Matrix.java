d89bd9d757793e70a66e7520998b3ff20f5d148f
markfront <markfront@gmail.com>
2020-07-13 09:54:44 -0700

    Update Longest_Increasing_Path_in_Matrix.java

--- src/Matrix/Longest_Increasing_Path_in_Matrix.java
+++ src/Matrix/Longest_Increasing_Path_in_Matrix.java
@@ -32,43 +32,52 @@ Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is n
 
 // Idea: DFS + memory. Kind of a DP.
 
-public int longestIncreasingPath(int[][] matrix) {
-    int result = 0;
-    int m = matrix.length;
-    int n = matrix[0].length;
- 
-    int[][] mem = new int[m][n];
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            int t = helper(matrix, mem, i, j);
-            result = Math.max(result, t);
+class Solution {
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix==null || matrix.length==0) return 0;
+        
+        int result = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] mem = new int[m][n]; // memory
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int t = helper(matrix, mem, i, j, m, n);
+
+                result = Math.max(result, t);
+            }
         }
+
+        return result;
     }
  
-    return result;
-}
- 
-private int helper(int[][] matrix, int[][] mem, int i, int j) {
-    if (mem[i][j] > 0) {
-        return mem[i][j];
-    }
- 
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
- 
-    for (int k = 0; k < 4; k++) {
-        int x = i + dx[k];
-        int y = j + dy[k];
- 
-        if (x >= 0 && y >= 0
-                && x < matrix.length
-                && y < matrix[0].length
-                && matrix[x][y] > matrix[i][j]) {
-            mem[i][j] = Math.max(mem[i][j], helper(matrix, mem, x, y));
-        } else {
-            // serve as base-case for recursion!!!
+    private int helper(int[][] matrix, int[][] mem, int i, int j, int m, int n) {
+        if (mem[i][j] > 0) {
+            return mem[i][j];
         }
+        
+        mem[i][j]=1; // add 1 for curr number
+
+        int[][] moves = new int[][] { {i-1, j}, {i+1, j}, {i, j-1}, {i, j+1} };
+
+        for (int k = 0; k < 4; k++) {
+            int x = moves[k][0];
+            int y = moves[k][1];
+
+            if (x >= 0 && y >= 0 && x < m && y < n
+                    && matrix[x][y] > matrix[i][j]
+               ) {
+                
+                mem[i][j] = Math.max(mem[i][j], 1 + helper(matrix, mem, x, y, m, n));
+                
+            } else {
+                // serve as base-case for recursion!!!
+            }
+        }
+        
+        return mem[i][j];        
     }
- 
-    return ++mem[i][j];
 }
