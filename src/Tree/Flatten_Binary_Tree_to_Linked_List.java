32a236ba54764d9fa8e7668cb5a8789fc25fd6c8
markfront <markfront@gmail.com>
2020-07-15 22:01:23 -0700

    Create Flatten_Binary_Tree_to_Linked_List.java

---
+++ src/Tree/Flatten_Binary_Tree_to_Linked_List.java
@@ -0,0 +1,63 @@
+/*
+https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+
+Given a binary tree, flatten it to a linked list in-place.
+
+For example, given the following tree:
+
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+
+The flattened tree should look like:
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root==null) return;
+        
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        
+        root.left = null;
+        
+        flatten(left);
+        flatten(right);
+        
+        root.right = left;
+        TreeNode curr = root;
+        while(curr.right!=null) {
+            curr = curr.right;
+        }
+        curr.right = right;        
+    }
+}
